caea8f676b1d65e193dac86c0843c7fccc0ea4a2
eniola4031 <agboola.eniola4031@gmail.com>
2022-01-15 10:41:28 +0100

    fixe

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
 import "hardhat/console.sol";
 
 contract NFTMarket is ReentrancyGuard {
